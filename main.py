f42178050d48540cbc18ee1b0573bb08176327ca
AlexBarth <alexbarth92@outlook.com>
2024-01-25 17:04:36 +0100

    reformat with black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-'''this script is the main script which triggers all necessary functions
+"""this script is the main script which triggers all necessary functions
 to pull current list of active devices via Meraki API and compares it to
-the latest information'''
+the latest information"""
 
 from compare_license import _meraki, _csv
 from o365 import _mail
@@ -9,20 +9,18 @@ import datetime as dt
 
 
 def main():
-    ''' this is the main function'''
+    """this is the main function"""
     # create new dict for result
     diff_all = {}
     # list of all producttype you wish to capture the licenses for
-    list_producttype = ['wireless', 'switch', 'appliance', 'cellularGateway']
+    list_producttype = ["wireless", "switch", "appliance", "cellularGateway"]
     # create new _meraki object for API access
     dashboard = _meraki()
     # capture licensed devices for all producttypes in list_producttype
     for producttype in list_producttype:
         diff_all.update(
-            **dashboard._get_count_devices(
-                dashboard._get_devices(producttype)
-                )
-            )
+            **dashboard._get_count_devices(dashboard._get_devices(producttype))
+        )
     # create new _csv object
     delta_csv = _csv()
     # write license information to new csv
@@ -31,29 +29,29 @@ def main():
     delta_csv.read_csv_reference()
 
     date = dt.date.today()
-    month = date.strftime('%B')
+    month = date.strftime("%B")
 
     # instantiate new mail object
     mail = _mail
     mail.authenticate()
     # create header dict
     header = {}
-    header['from'] = config('FROM')
-    header['to'] = config('TO')
-    header['cc'] = str(config('CC')).split(',')
-    header['subject'] = f'license overview for the month {month}'
+    header["from"] = config("FROM")
+    header["to"] = config("TO")
+    header["cc"] = str(config("CC")).split(",")
+    header["subject"] = f"license overview for the month {month}"
     # create body text
-    body = f'''
+    body = f"""
     Dear receiver,
     attached you can find the license overview for the month {month}
-    '''
+    """
     # update created csv with delta of last created csv and attach to mail
-    header['file'] = delta_csv.write_csv_delta()
+    header["file"] = delta_csv.write_csv_delta()
     # send mail
     mail.send_mail(header, body)
     # finished
-    print('data was processed.')
+    print("data was processed.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
